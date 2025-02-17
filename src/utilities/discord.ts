de169de8ff6451fef86e869159fbf6439460aecc
Matt Hatcher <matthatcher@pm.me>
2025-02-17 12:17:36 -0500

    style: spaces > tabs

--- src/utilities/discord.ts
+++ src/utilities/discord.ts
@@ -1,27 +1,27 @@
 export function respond(response: unknown, statusCode = 200) {
-	return {
-		statusCode,
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify(response),
-	};
+  return {
+    statusCode,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(response),
+  };
 }
 
 export async function sendRequestToDiscord(
-	endpoint: string,
-	method: "GET" | "POST" | "PATCH" | "DELETE" | "PUT",
-	body?: unknown,
+  endpoint: string,
+  method: "GET" | "POST" | "PATCH" | "DELETE" | "PUT",
+  body?: unknown,
 ) {
-	const response = await fetch(`https://discord.com/api/v10/${endpoint}`, {
-		method,
-		headers: {
-			Authorization: `Bot ${process.env.DISCORD_BOT_TOKEN}`,
-			"Content-Type": "application/json",
-			"User-Agent": "DiscordBot (hardcarry.club, 1.0.0)",
-		},
-		body: JSON.stringify(body) ?? undefined,
-	});
+  const response = await fetch(`https://discord.com/api/v10/${endpoint}`, {
+    method,
+    headers: {
+      Authorization: `Bot ${process.env.DISCORD_BOT_TOKEN}`,
+      "Content-Type": "application/json",
+      "User-Agent": "DiscordBot (hardcarry.club, 1.0.0)",
+    },
+    body: JSON.stringify(body) ?? undefined,
+  });
 
-	return response.json();
+  return response.json();
 }
