763c837249c48e0da7269f74724ebe744836f289
Matt Hatcher <matthatcher@pm.me>
2025-02-17 11:56:33 -0500

    feat: initial source plus some extra

---
+++ src/utilities/discord.ts
@@ -0,0 +1,27 @@
+export function respond(response: unknown, statusCode = 200) {
+	return {
+		statusCode,
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(response),
+	};
+}
+
+export async function sendRequestToDiscord(
+	endpoint: string,
+	method: "GET" | "POST" | "PATCH" | "DELETE" | "PUT",
+	body?: unknown,
+) {
+	const response = await fetch(`https://discord.com/api/v10/${endpoint}`, {
+		method,
+		headers: {
+			Authorization: `Bot ${process.env.DISCORD_BOT_TOKEN}`,
+			"Content-Type": "application/json",
+			"User-Agent": "DiscordBot (hardcarry.club, 1.0.0)",
+		},
+		body: JSON.stringify(body) ?? undefined,
+	});
+
+	return response.json();
+}
---
+++ src/utilities/env.ts
@@ -0,0 +1,6 @@
+export const BOT_TOKEN = process.env.DISCORD_BOT_TOKEN;
+export const APP_ID = process.env.DISCORD_APP_ID;
+export const PUBLIC_KEY = process.env.DISCORD_PUBLIC_KEY;
+
+export const GUILD_ID = "941819133576765492";
+export const PUG_BANNED_ROLE_ID = "1327429519052640339";
