3a520e4add25a2c5eab516fc34f73beac4fbe282
Matt Hatcher <matthatcher@pm.me>
2025-09-09 02:31:32 -0400

    feat: add game column to user notes and discord messages tables, refactor table names

---
+++ drizzle/0002_add_game_to_notes.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `pug_user_note_discord_messages` ADD `game` text NOT NULL;--> statement-breakpoint
+ALTER TABLE `pug_user_notes` ADD `game` text NOT NULL;
---
+++ drizzle/0003_refactor_table_names.sql
@@ -0,0 +1,4 @@
+ALTER TABLE `pug_bans` RENAME TO `pug_ban`;--> statement-breakpoint
+ALTER TABLE `pug_lobbies` RENAME TO `pug_lobby`;--> statement-breakpoint
+ALTER TABLE `pug_user_notes` RENAME TO `pug_user_note`;--> statement-breakpoint
+ALTER TABLE `pug_user_note_discord_messages` RENAME TO `pug_user_note_discord_message`;
