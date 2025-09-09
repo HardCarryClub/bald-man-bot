4c2495300e7fe798baed273030735aa849396b09
Matt Hatcher <matthatcher@pm.me>
2025-09-09 04:06:02 -0400

    Revert "fix: add the column after the user_id"
    This reverts commit 9f51b7f28315bfc71f834d4622da1ab1a06aec2a.

--- drizzle/0002_add_game_to_notes.sql
+++ drizzle/0002_add_game_to_notes.sql
@@ -1,2 +1,2 @@
-ALTER TABLE `pug_user_note_discord_messages` ADD `game` text NOT NULL;
-ALTER TABLE `pug_user_notes` ADD `game` text NOT NULL AFTER `user_id`;
+ALTER TABLE `pug_user_note_discord_messages` ADD `game` text NOT NULL;--> statement-breakpoint
+ALTER TABLE `pug_user_notes` ADD `game` text NOT NULL;
