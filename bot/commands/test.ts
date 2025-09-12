e518c6fcce461e15a2632c0b829930249d7eadb2
Matt Hatcher <matthatcher@pm.me>
2025-09-12 02:04:40 -0400

    test command updates

--- bot/commands/test.ts
+++ bot/commands/test.ts
@@ -1,6 +1,9 @@
-import { signupMessageComponents } from "@bot/utilities/host-scheduling";
+/** biome-ignore-all lint/style/noNonNullAssertion: Testing */
+import { db } from "@app/db";
+import { pugLobbyHostSignup } from "@app/db/schema";
+import { createSignupRecord, type HostSignup, signupComponents } from "@bot/utilities/host-scheduling";
 import { MessageFlags } from "discord-api-types/v10";
-import type { CommandConfig, CommandInteraction } from "dressed";
+import { type CommandConfig, type CommandInteraction, createMessage, editMessage, TextDisplay } from "dressed";
 
 export const config: CommandConfig = {
   description: "Test command",
@@ -9,8 +12,47 @@ export const config: CommandConfig = {
 };
 
 export default async function (interaction: CommandInteraction) {
-  await interaction.reply({
-    components: signupMessageComponents("overwatch") || [],
+  const message = await createMessage(interaction.channel.id, {
+    components: [TextDisplay("Setting up host signup...")],
     flags: MessageFlags.IsComponentsV2,
   });
+
+  const record = createSignupRecord("rivals");
+  const result = await db
+    .insert(pugLobbyHostSignup)
+    .values({
+      game: "rivals",
+      channelId: message.channel_id,
+      messageId: message.id,
+      data: JSON.stringify(record),
+      createdAt: new Date().toISOString(),
+      createdBy: interaction.user.id,
+    })
+    .returning({ id: pugLobbyHostSignup.id });
+
+  const signup: HostSignup = {
+    id: result[0]!.id,
+    game: "rivals",
+    channelId: message.channel_id,
+    messageId: message.id,
+    data: record!,
+  };
+
+  const components = signupComponents(signup);
+
+  if (!components) {
+    return interaction.reply({ content: "Failed to generate signup components.", ephemeral: true });
+  }
+
+  await editMessage(message.channel_id, message.id, {
+    components,
+    flags: MessageFlags.IsComponentsV2,
+  });
+
+  await interaction.reply({ content: "Host signup setup complete.", ephemeral: true });
+
+  // await interaction.reply({
+  //   components: signupComponents(signup) || [],
+  //   flags: MessageFlags.IsComponentsV2,
+  // });
 }
